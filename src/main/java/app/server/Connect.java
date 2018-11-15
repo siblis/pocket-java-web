f145abb293f5653fefd868f333302f842329ab85
abelyackov <abelyackov@yandex.ru>
2018-11-15 09:47:46 +0300

    Внес изменения в метод авторизации: При ответе от сервера с кодом 200, получает токен, используя метод getToken класса ResponseAuth. Возвращает строку с ответом от сервера в формате "Код + сообщение".

--- src/main/java/app/server/Connect.java
+++ src/main/java/app/server/Connect.java
@@ -1,6 +1,10 @@
 //written by Grigory
+//edited by Artem
 package app.server;
 
+import app.responseProcessing.ResponseAuth;
+
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -8,7 +12,8 @@ import java.net.URL;
 public class Connect {
     private static String serverURL = "https://pocketmsg.ru:8888";
 
-    public static void authorization(String JsonNamePassword) throws Exception {
+    public static String authorization(String JsonNamePassword) throws Exception {
+        String token = null;
         URL url = new URL(serverURL + "/v1/auth/");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("PUT");
@@ -19,20 +24,23 @@ public class Connect {
         stream.close();
 
         int responseCode = connection.getResponseCode();
-        System.out.println("Отправлено " + connection.getRequestMethod() + "URL " + connection.getURL());
-        System.out.println("Response code " + responseCode);
-        System.out.println(connection.getResponseMessage());
-    }
-
-    public Connect(String jsonNamePassword) {
-
-        try {
-            authorization(jsonNamePassword);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (responseCode == 200) {
+            DataInputStream inputStream = new DataInputStream(connection.getInputStream());
+            token = ResponseAuth.getToken(inputStream.readLine());
         }
+        return (responseCode + " " + connection.getResponseMessage());
+
     }
+
+//    public Connect(String jsonNamePassword) {
+//
+//        try {
+//            authorization(jsonNamePassword);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     //  {
 //    "account_name": username,
