bc195ed3a32df43f9d031a976553a023aaf89ea5
abelyackov <abelyackov@yandex.ru>
2018-11-19 19:49:57 +0300

    немного изменил условие проверки ответов

--- src/main/java/app/server/responseProcessing/ResponseAuth.java
+++ src/main/java/app/server/responseProcessing/ResponseAuth.java
@@ -9,23 +9,23 @@ public class ResponseAuth {
     public static String resp(String respFromServer) {
         int respCode = Integer.parseInt(respFromServer.split(" ", 2)[0]);
         String respMSG = respFromServer.split(" ", 2)[1];
-        if (respCode != 200)
-            if (respCode == 403 && respMSG.equals("Login or password incorrect")) {
-                return "Неверное имя пользователя/пароль";
-            } else if (respCode == 403 && respMSG.equals("Incorrect password")) {
-                return "Неверный пароль";
-            }
-        return "OK";
+        if (respCode == 200) return "OK";
+        if (respCode == 403 && respMSG.equals("Login or password incorrect")) {
+            return "Неверное имя пользователя/пароль";
+        }
+        if (respCode == 403 && respMSG.equals("Incorrect password")) {
+            return "Неверный пароль";
+        }
+        return null;
     }
 
     public static String getToken(String response) throws IOException {
-
         JsonFactory factory = new JsonFactory();
-        JsonParser parser  = factory.createParser(response);
+        JsonParser parser = factory.createParser(response);
 
-        while(!parser.isClosed()){
+        while (!parser.isClosed()) {
             JsonToken jsonToken = parser.nextToken();
-            if(jsonToken.VALUE_STRING.equals(jsonToken)) {
+            if (jsonToken.VALUE_STRING.equals(jsonToken)) {
 //                System.out.println("Token: " + parser.getValueAsString());
                 return parser.getValueAsString();
             }
